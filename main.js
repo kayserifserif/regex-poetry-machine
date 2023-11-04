279621d10c0438aeb5f658c38b2192ce777d66de
Katherine Yang <whykatherine@gmail.com>
2023-11-03 23:54:13 -0400

    add alternation token

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import "./style.css";
 
-import { AlternationToken, BracketToken, CharacterToken, GroupToken } from "./tokens";
+import { AlternationToken, BracketToken, CharacterToken, GroupToken, OptionalToken } from "./tokens";
 
 const PATTERN_TEMPLATE = document.querySelector(".pattern");
 PATTERN_TEMPLATE.remove();
@@ -448,161 +448,181 @@ function surrogateSlice(str, start, end) {
 
 //   console.log(tokens)
 // }
-// function tokenise(pattern) {
-//   const tokens = [];
-//   const chars = Array.from(pattern);
-//   for (let i = 0; i < chars.length; i++) {
-//     const char = chars[i];
-//     switch (char) {
-//       case "[":
-//         {
-//           const startIndex = i;
-//           let endIndex = chars.findIndex((c, i) => {
-//             if (i <= startIndex) return false;
-//             if (c === "]") return true;
-//             return false;
-//           });
-//           if (endIndex === -1) {
-//             // no end bracket
-//             throw new Error(`Encountered opening bracket '[' but no matching closing bracket ']'\n
-// ${pattern}
-// ${' '.repeat(startIndex)}^
-// at index ${startIndex}`);
-//           }
-//           const bracket = chars.slice(startIndex, endIndex + 1);
-//           let bracketChars = bracket.slice(1, bracket.length - 1);
-//           const charTokens = bracketChars.map(c => new CharacterToken(c));
-//           const token = new BracketToken(charTokens);
-//           tokens.push(token);
-//           i = endIndex + 1;
-//         }
-//         break;
-//       case "(":
-//         {
-//           const startIndex = i;
-//           let endIndex = chars.findIndex((c, i) => {
-//             if (i <= startIndex) return false;
-//             if (c === ")") return true;
-//             return false;
-//           });
-//           if (endIndex === -1) {
-//             // no end paren
-//             throw new Error(`Encountered opening paren '(' but no matching closing paren ')'\n
-// ${pattern}
-// ${' '.repeat(startIndex)}^
-// at index ${startIndex}`);
-//           }
-//           const group = chars.slice(startIndex, endIndex + 1);
-//           let groupChars = group.slice(1, group.length - 1);
-//           const charTokens = tokenise(groupChars.join(""));
-//           // const charTokens = groupChars.map(c => new CharacterToken(c));
-//           const token = new GroupToken(charTokens);
-//           tokens.push(token);
 
-//           i = endIndex + 1;
-//         }
-//         break;
-//       case "|":
-//         {
-//           let prevToken;
-//           if (tokens.length > 0) {
-//             prevToken = tokens[tokens.length - 1];
-//           }
-//           const innerTokens = [prevToken];
-//           const startIndex = i;
-//           let endIndex = chars.length - 1;
-//           for (let j = startIndex + 1; j < chars.length; j++) {
-//             const c = chars[j];
-//             if (c === ")") {
-//               endIndex = j;
-//               break;
-//             }
-//           }
-//           // console.log(chars.slice(startIndex, endIndex + 1));
-//           const remainder = chars.slice(startIndex + 1, endIndex + 1).join("");
-//           const nextToken = tokenise(remainder);
-//           innerTokens.push(nextToken);
-//           console.log(innerTokens)
-//           const token = new AlternationToken(innerTokens);
-//           tokens.push(token);
+function tokenise(pattern) {
+  const tokens = [];
+  const chars = Array.from(pattern);
+  for (let i = 0; i < chars.length; i++) {
+    const char = chars[i];
+    switch (char) {
+      case "[":
+        {
+          const startIndex = i;
+          let endIndex = chars.findIndex((c, i) => {
+            if (i <= startIndex) return false;
+            if (c === "]") return true;
+            return false;
+          });
+          if (endIndex === -1) {
+            // no end bracket
+            throw new Error(`Encountered opening bracket '[' but no matching closing bracket ']'\n
+${pattern}
+${' '.repeat(startIndex)}^
+at index ${startIndex}`);
+          }
+          const bracket = chars.slice(startIndex, endIndex + 1);
+          let bracketChars = bracket.slice(1, bracket.length - 1);
+          const charTokens = bracketChars.map(c => new CharacterToken(c));
+          const token = new BracketToken(charTokens);
+          tokens.push(token);
+          i = endIndex + 1;
+        }
+        break;
+      case "(":
+        {
+          const startIndex = i;
+          let endIndex = chars.findIndex((c, i) => {
+            if (i <= startIndex) return false;
+            if (c === ")") return true;
+            return false;
+          });
+          if (endIndex === -1) {
+            // no end paren
+            throw new Error(`Encountered opening paren '(' but no matching closing paren ')'\n
+${pattern}
+${' '.repeat(startIndex)}^
+at index ${startIndex}`);
+          }
+          const group = chars.slice(startIndex, endIndex + 1);
+          let groupChars = group.slice(1, group.length - 1);
+          const charTokens = tokenise(groupChars.join(""));
+          // const charTokens = groupChars.map(c => new CharacterToken(c));
+          const token = new GroupToken(charTokens);
+          tokens.push(token);
 
-//           i = endIndex + 1;
-//         }
-//         break;
-//       default:
-//         {
-//           const token = new CharacterToken(char);
-//           tokens.push(token);
-//         }
-//         break;
-//     }
-//   }
-//   console.log(tokens)
-// }
-// // tokenise("se[ab]d")
-// // tokenise("se(ab)d")
-// // tokenise("se(a|b)d")
+          i = endIndex + 1;
+        }
+        break;
+      case "|":
+        {
+          let prevToken;
+          if (tokens.length > 0) {
+            prevToken = tokens[tokens.length - 1];
+          } else {
+            throw new Error("Looking for token before alternation operator '|' but couldn't find one.");
+          }
 
-// function generateStringsFromRegex(pattern) {
-//   let strings = [];
-//   let stringInProgress = "";
+          let innerTokens = [prevToken];
+          tokens.splice(tokens.length - 1, 1);
+          const startIndex = i;
+          let endIndex = chars.length - 1;
+          for (let j = startIndex + 1; j < chars.length; j++) {
+            const c = chars[j];
+            if (c === ")") {
+              endIndex = j;
+              break;
+            }
+          }
+          // console.log(chars.slice(startIndex, endIndex + 1));
+          const remainder = chars.slice(startIndex + 1, endIndex + 1).join("");
+          const nextToken = tokenise(remainder);
+          // innerTokens.push(nextToken);
+          innerTokens = innerTokens.concat(nextToken);
+          const token = new AlternationToken(innerTokens);
+          tokens.push(token);
+
+          i = endIndex + 1;
+        }
+        break;
+      case "?":
+        {
+          let prevToken;
+          if (tokens.length > 0) {
+            prevToken = tokens[tokens.length - 1];
+          } else {
+            throw new Error("Looking for token before optional operator '?' but couldn't find one.");
+          }
+          tokens.splice(tokens.length - 1, 1);
+          const token = new OptionalToken(prevToken);
+          tokens.push(token);
+        }
+        break;
+      default:
+        {
+          const token = new CharacterToken(char);
+          tokens.push(token);
+        }
+        break;
+    }
+  }
+  return tokens;
+}
+// tokenise("se[ab]d")
+// tokenise("se(ab)d")
+// tokenise("se(a|b)d")
+// tokenise("mou?rning")
+tokenise("f(l?ight|reeze)")
+
+function generateStringsFromRegex(pattern) {
+  let strings = [];
+  let stringInProgress = "";
   
-//   const chars = Array.from(pattern);
-//   let i = 0;
-//   for (; i < chars.length; i++) {
-//     const char = chars[i];
-//     switch (char) {
-//       case "[":
-//         {
-//           const returnedStrings = parseBracket(pattern, i, stringInProgress);
-//           returnedStrings.forEach(str => strings.push(str));
-//           return strings;
-//         }
-//       default:
-//         stringInProgress += char;
-//         break;
-//     }
-//   }
+  const chars = Array.from(pattern);
+  let i = 0;
+  for (; i < chars.length; i++) {
+    const char = chars[i];
+    switch (char) {
+      case "[":
+        {
+          const returnedStrings = parseBracket(pattern, i, stringInProgress);
+          returnedStrings.forEach(str => strings.push(str));
+          return strings;
+        }
+      default:
+        stringInProgress += char;
+        break;
+    }
+  }
 
-//   if (stringInProgress) {
-//     strings.push(stringInProgress);
-//   }
+  if (stringInProgress) {
+    strings.push(stringInProgress);
+  }
 
-//   return strings;
-// }
+  return strings;
+}
 
-// function parseBracket(pattern, startIndex, stringInProgress) {
-//   // se[ea]
-//   let strings = [];
-//   const chars = Array.from(pattern);
-//   let endIndex = chars.findIndex((c, i) => {
-//     if (i <= startIndex) return false;
-//     if (c === "]") return true;
-//     return false;
-//   });
-//   if (endIndex === -1) {
-//     // no end bracket
-//     // endIndex = chars.length - 1;
-//     // blah;
-//     throw new Error(`Encountered opening bracket '[' but no matching closing bracket ']'\n
-// ${pattern}
-// ${' '.repeat(startIndex)}^
-// at index ${startIndex}`);
-//   }
+function parseBracket(pattern, startIndex, stringInProgress) {
+  // se[ea]
+  let strings = [];
+  const chars = Array.from(pattern);
+  let endIndex = chars.findIndex((c, i) => {
+    if (i <= startIndex) return false;
+    if (c === "]") return true;
+    return false;
+  });
+  if (endIndex === -1) {
+    // no end bracket
+    // endIndex = chars.length - 1;
+    // blah;
+    throw new Error(`Encountered opening bracket '[' but no matching closing bracket ']'\n
+${pattern}
+${' '.repeat(startIndex)}^
+at index ${startIndex}`);
+  }
 
-//   const bracket = chars.slice(startIndex, endIndex);
-//   const bracketChars = bracket.slice(1, bracket.length);
+  const bracket = chars.slice(startIndex, endIndex);
+  const bracketChars = bracket.slice(1, bracket.length);
   
-//   // new paths based on characters in bracket
-//   bracketChars.forEach(c => {
-//     const tempString = stringInProgress + c + chars.slice(endIndex + 1).join("");
-//     const returnedStrings = generateStringsFromRegex(tempString);
-//     returnedStrings.forEach(str => strings.push(str));
-//   });
-//   return strings;
-// }
+  // new paths based on characters in bracket
+  bracketChars.forEach(c => {
+    const tempString = stringInProgress + c + chars.slice(endIndex + 1).join("");
+    const returnedStrings = generateStringsFromRegex(tempString);
+    returnedStrings.forEach(str => strings.push(str));
+  });
+  return strings;
+}
 
-// // console.log(generateStringsFromRegex("[mtb]ake"));
-// // console.log(generateStringsFromRegex("be[an]d"));
-// // console.log(generateStringsFromRegex("hai[lr]"));
-// // console.log(generateStringsFromRegex("be[ab]n[ab]"))
+// console.log(generateStringsFromRegex("[mtb]ake"));
+// console.log(generateStringsFromRegex("be[an]d"));
+// console.log(generateStringsFromRegex("hai[lr]"));
+// console.log(generateStringsFromRegex("be[ab]n[ab]"))
